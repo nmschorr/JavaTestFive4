5637f89ea3a1d0ec77eea2822317a72349fdc14e
nmschorr <nancyschorr@yahoo.com>
2017-10-13 20:17:03 -0700

    updated again

--- JsonTestTwo/src/main/java/com/nmschorr/JavaTestFive.java
+++ JsonTestTwo/src/main/java/com/nmschorr/JavaTestFive.java
@@ -32,21 +32,18 @@ public class JavaTestFive {
 		System.out.println("\nRunning readUrlData");
 		String locUrlString = "http://jsonplaceholder.typicode.com/albums";
 		HttpURLConnection newUrlConn = null;
-
 		URL urlObj = new URL(locUrlString);
 		newUrlConn = (HttpURLConnection) urlObj.openConnection();
 		newUrlConn.setRequestMethod("GET");
-		newUrlConn.setConnectTimeout(5000);
-		newUrlConn.setReadTimeout(5000);
+		newUrlConn.setConnectTimeout(6000);
+		newUrlConn.setReadTimeout(6000);
 		newUrlConn.setRequestProperty("Accept", "application/json");
 		if (newUrlConn.getResponseCode() != HttpURLConnection.HTTP_OK) {
 			System.err.println("Can't connect to the URL...");
 		}
-		System.out.println("Connected to the server...");
-
+		System.out.println("Connected to the web server...");
 		InputStream is = newUrlConn.getInputStream();
 		Reader bufReader = new BufferedReader(new InputStreamReader((is)));            
-
 		StringBuilder myStrBuilder = new StringBuilder();
 		while((builderLine = ((BufferedReader)bufReader).readLine()) != null) {
 			myStrBuilder.append(builderLine);
